16427423214a2cbd60555f23f6f5d16bc5a9b702
Beachnewt <beachnate10@gmail.com>
2023-02-22 16:46:44 -0600

    2/22 Coding In-Class
    Changes from class on 2/22

--- Mission Demolition/Assets/Slingshot.cs
+++ Mission Demolition/Assets/Slingshot.cs
@@ -4,25 +4,70 @@ using UnityEngine;
 
 public class Slingshot : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    // Header tells compiler to only set this in editor
+    // However this isn't necessary since LaunchPoint is already under slingshot
+    [Header("Inscribed")]
+    public GameObject projectilePrefab;
+    public float velocityMult = 10f;
+
+    // Instnatiates with Awake
+    [Header("Dynamic")]
+    public GameObject launchPoint;
+    public GameObject projectile; // singleton; only want one of at any time
+    public bool aimingMode; // defining state based machine
+
+    void Awake()
     {
-        
+        launchPoint = transform.Find("LaunchPoint").gameObject;
+        launchPoint.SetActive(false);
+        aimingMode = false;
     }
 
-    // Update is called once per frame
-    void Update()
+    void Update() 
     {
-        
+        // PRE-CONDITION: if aimingMode, then projectile is instantiated
+        if (!aimingMode)
+        {
+            return;
+        }
+
+        Vector3 mousePos2D = Input.mousePosition;
+        mousePos2D.z = -Camera.main.transform.position.z;
+        Vector3 mousePos3D = Camera.main.ScreenToWorldPoint(mousePos2D);
+
+        Vector3 mouseDelta = mousePos3D - launchPoint.transform.position;
+        float maxMagnitude = this.GetComponent<SphereCollider>().radius;
+        if (mouseDelta.magnitude > maxMagnitude)
+        {
+            mouseDelta.Normalize();
+            mouseDelta *= maxMagnitude;
+        }
+
+        Vector3 projPos = launchPoint.transform.position + mouseDelta;
+        // PRE-CONDITION: projectile needs to be instantiated
+        projectile.transform.position = projPos;
     }
 
     void OnMouseEnter()
     {
-        print("Slingshot::OnMouseEnter()");
+        // print("Slingshot::OnMouseEnter()");
+        // SetActive toggles but Halo is iffy so code below doesn't work
+        // launchPoint.GetComponent<Halo>.SetActive(true);
+        launchPoint.SetActive(true);
     }
 
     void OnMouseExit() 
     {
-        print("Slingshot::OnMouseExit()");
+        // print("Slingshot::OnMouseExit()");
+        launchPoint.SetActive(false);
+    }
+
+    void OnMouseDown()
+    {
+        aimingMode = true; // POST-CONDITIONS: Projectile must be instantiated
+        projectile = Instantiate<GameObject>(projectilePrefab);
+        projectile.transform.position = launchPoint.transform.position;
+        projectile.GetComponent<Rigidbody>().isKinematic = true;
     }
 }
